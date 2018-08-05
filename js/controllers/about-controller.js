49539aa4ef52f54bbbb90fc6194b60b64c37f9b1
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-08-06 00:36:51 +0700

    1

--- js/controllers/about-controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('kiarash')
-.controller('AboutController', function($scope) {
-
-});
--- js/controllers/blogdetail-Controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('kiarash')
-.controller('BlogdetailController', function($scope,$rootScope,$routeParams,$filter,$sce) {
-    
-       $scope.detail = $filter('filter')($rootScope.news, {id:$routeParams.id});
-        console.log( $scope.detail);
-       $scope.description = $sce.trustAsHtml($scope.detail[0].description);
-       $scope.title = $sce.trustAsHtml($scope.detail[0].title);
-    
-})
